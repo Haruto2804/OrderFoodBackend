f189943103d76e5e6deb98febf4412f7f2e5e1df
HaruSama2804 <baosenpai2005@gmail.com>
2026-06-29 09:35:01 +0700

    prepare deploy backend into Internet by Render

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { type Request, type Response } from "express";
+import * as dotenv from "dotenv"; // 1. Import dotenv
+dotenv.config();
 import mongoose from "mongoose";
 import {
   getMenuItems,
@@ -20,7 +22,7 @@ import {
   updateOrderStatus,
 } from "./controllers/order.controller.js";
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Ưu tiên lấy PORT của hệ thống cấp
 app.use(express.json());
 
 // MENU ROUTES
@@ -41,9 +43,14 @@ app.get("/api/order", getOrders);
 app.post("/api/order", createOrder);
 app.delete("/api/order/:orderId", deleteOrder);
 app.patch("/api/order/status/:orderId", updateOrderStatus);
-
-mongoose
-  .connect("mongodb://localhost:27017/Food_Server")
+const mongoURL = process.env.MONGODB_URL;
+if (!mongoURL) {
+  throw new Error(
+    "LỖI: Biến MONGODB_URI chưa được định nghĩa trong file .env!",
+  );
+}
+await mongoose
+  .connect(mongoURL)
   .then(() => {
     console.log("Đã kết nối với MongoDB");
     app.listen(PORT, () => {
